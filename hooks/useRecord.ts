0cca24cdfe8840fa90e3fd967a06f00aa559562b
frankiescot788 <scottfrankie864@gmail.com>
2024-12-05 12:51:34 +0200

    updated components

--- hooks/useRecord.ts
+++ hooks/useRecord.ts
@@ -12,6 +12,8 @@ import { Svg, Path } from "react-native-svg";
 import { LinearGradient } from "expo-linear-gradient";
 import * as filesystem from "expo-file-system";
 import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Rec_storage, Recording } from "@/types/types";
 
 export default function useRecord() {
   const [isRecording, setIsrecording] = useState<any | null>(null);
@@ -21,6 +23,8 @@ export default function useRecord() {
   const incrementRef = useRef<number>(0);
   const [onOpen, setOpen] = useState<boolean>(false);
   const [audioName, setAudioname] = useState<string>("");
+
+  const [recordings, setRecordings] = useState([]);
 
   const router = useRouter();
 
@@ -54,7 +58,6 @@ export default function useRecord() {
       const status = await isRecording.getStatusAsync();
       const minutes = Math.floor(status.durationMillis / 60000);
       const seconds = Math.floor((status.durationMillis % 60000) / 1000);
-
       setDuration(`${minutes}:${seconds < 10 ? "0" : ""}${seconds}`);
     } catch (error) {
       console.log(error);
@@ -80,7 +83,19 @@ export default function useRecord() {
         `${filesystem.documentDirectory}/recordings/`
       );
       const m4s = fileList.filter((file) => file.endsWith("m4a"));
-      console.log(m4s);
+      const items: any = await AsyncStorage.getItem("recordings");
+
+      const parse = JSON.parse(items) || [];
+      setRecordings(parse);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const delete_recordings = async () => {
+    try {
+      await AsyncStorage.removeItem("recordings");
+      console.log("deleted");
     } catch (error) {
       console.log(error);
     }
@@ -92,7 +107,70 @@ export default function useRecord() {
         from: audioURI,
         to: `${filesystem.documentDirectory}/recordings/${audioName}.m4a`,
       });
+
+      const prevItems: any = await AsyncStorage.getItem("recordings");
+
+      const parsedData = JSON.parse(prevItems) || [];
+
+      const new_recording = {
+        name: audioName,
+        duration,
+        audioURI,
+        createdAt: new Date().toISOString(),
+      };
+
+      await AsyncStorage.setItem(
+        "recordings",
+        JSON.stringify([...parsedData, new_recording])
+      );
+
       console.log("saved");
+      router.navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const get_recording = async (uri: string) => {
+    try {
+      const recordings: any = await AsyncStorage.getItem("recordings");
+
+      const parse_files = JSON.parse(recordings) || [];
+
+      const file_info = parse_files.filter(
+        (file: Rec_storage) => file.name === uri
+      );
+
+      if (!file_info) return;
+
+      await sound.unloadAsync();
+      await sound.loadAsync({ uri: file_info.at(0).audioURI });
+      await sound.playAsync();
+
+      console.log("playing");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const unload_recording = async () => {
+    try {
+      await sound.stopAsync();
+      console.log("stopped");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const delete_recording = async (uri : string) => {
+    try {
+      const get_records : any = await AsyncStorage.getItem("recordings");
+      const parse_recordings : Recording[] = JSON.parse(get_records) || [];
+
+      const record_delete = parse_recordings.filter(rec => rec.name !== uri);
+
+      await AsyncStorage.setItem("recordings", JSON.stringify(record_delete));
+
       router.navigate("/");
     } catch (error) {
       console.log(error);
@@ -112,6 +190,11 @@ export default function useRecord() {
     audioURI,
     sound,
     audioName,
-    setAudioname
-  }
+    setAudioname,
+    recordings,
+    delete_recordings,
+    get_recording,
+    unload_recording,
+    delete_recording,
+  };
 }
